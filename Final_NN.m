4b7047879761e6bce21192cc765b330cb103f2e7
Jonathan Ashbrock <38219924+JDAshbrock@users.noreply.github.com>
2018-04-11 13:58:44 -0500

    Update Final_NN.m
    Added intermittent accuracy testing,

--- Final_NN.m
+++ Final_NN.m
@@ -4,17 +4,20 @@ clc
 
 PictureHeight=28;
 PictureWidth = 28;
+scale=16;
 
 InputSize = PictureHeight*PictureWidth+1; % +1 for the bias
 Layer1Size = 20;
 NumberOfCategories = 10; % 1 per digit from 0-9
 alpha = 1; %Scaling parameter so as to interpret gradients as probabilities
-LearningRate=0.05;
+BaseRate=0.1;
 ImageSet = LoadMNISTImages('train-images.idx3-ubyte');
 Labels = LoadMNISTLabels('train-labels.idx1-ubyte');
 TestImages = LoadMNISTImages('t10k-images.idx3-ubyte');
 TestLabels = LoadMNISTLabels('t10k-labels.idx1-ubyte');
-TestSize = 10000;
+TestSize = 5000;
+looplength=60000; %Control the length of the training loop - 60000 max.
+TotalWeights=InputSize*Layer1Size + (Layer1Size+1)*NumberOfCategories;
 
 Layer1Weights = zeros(Layer1Size,InputSize); %Layer1(i,j) contains the weight from input j into neuron input
 FinalWeights=zeros(NumberOfCategories,Layer1Size+1); %FinalWeights(i,j) contains weight from neuron j into category i
@@ -26,13 +29,15 @@ FinalWeights=zeros(NumberOfCategories,Layer1Size+1); %FinalWeights(i,j) contains
 %Initialize Layer 1 weights to a Bernoulli r.v. with p=0.5
 for i=1:Layer1Size
 	for j=1:InputSize
-		Layer1Weights(i,j) = 0.5*rand-0.25;
+        rv = randi([-scale,scale]);
+        Layer1Weights(i,j)=rv/scale;
 	end
 end
 %Initialize Final layer weights as a Bernoulli r.v. with p=0.5
 for i=1:NumberOfCategories
-	for j=1:Layer1Size
-		FinalWeights(i,j)=0.5*rand-0.25;
+	for j=1:Layer1Size+1
+        rv=randi([-scale,scale]);
+        FinalWeights(i,j)=rv/scale;
 	end
 end
 
@@ -40,8 +45,17 @@ Layer1Outputs = zeros(1,Layer1Size); %Layer1Outputs(i) contains the output form 
 FinalOutputs = zeros(1,NumberOfCategories); %FinalOutputs(i) contains the output corresponding to category i
 
 
-for l=1:60000%Training Loop
-    
+% countoff=zeros(1,looplength);
+% counton = zeros(1,looplength);
+% oversat=zeros(1,looplength);
+% undersat=zeros(1,looplength);
+sup=0;
+decay=1;
+numchanged=zeros(1,looplength);
+PartialAcc = zeros(1,floor(looplength/5000)); testnum=0; %These variables are used for intermittent accuracy test
+for l=1:looplength%Training Loop
+    decay=l/3000+1;
+    LearningRate=BaseRate/decay;
 	Im = horzcat(ImageSet(:,l)',1)';
     LabelVector=zeros(NumberOfCategories,1);
     ind = Labels(l)+1;
@@ -65,37 +79,55 @@ for l=1:60000%Training Loop
             if(j~= Layer1Size+1)
                 NeuralOutputGradients(j)=NeuralOutputGradients(j)+FinalGradient(i,j)*(FinalWeights(i,j)/Layer1Outputs(j));
             end
-        end
-       
-	end
+        end 
+        
+    end
+    
 	% This loop iterates over all weights in the initial Layer
 	% It first computes the gradient with respect to each of the weights
 	% Then it updates the weights as described in the algorithm
 	for i=1:Layer1Size %#ok<ALIGN>
 		for j=1:InputSize %#ok<ALIGN>
 			temp = Layer1Weights(i,:)*Im;
-			Layer1Gradient(i,j)= NeuralOutputGradients(i)*(1/(pi+pi*temp*temp))*Im(j);
+			Layer1Gradient(i,j)= NeuralOutputGradients(i)*(1/(pi+pi*temp*temp))*Im(j);            
         end
     end
-
-    Layer1Weights = Layer1Weights -LearningRate.*Layer1Gradient;
-    FinalWeights = FinalWeights - LearningRate.*FinalGradient;
+    %Update
+    Layer1Weights = Layer1Weights-LearningRate.*Layer1Gradient;
+    FinalWeights = FinalWeights-LearningRate.*FinalGradient;
+    sup1 = max(max(Layer1Gradient));
+    sup2 =max(max(FinalGradient));
+    sup=max([sup1,sup2,sup]); % This code tracks the size of the largest gradient value
+    
     if(rem(l,1000)==1)
         l
     end
-end
+    % Test intermittently throughout training
+    if(rem(l,1000)==0)
+        counter=0;
+        testnum = testnum+1;
+        for i=1:TestSize
+        Im = horzcat(TestImages(:,i)',1)';
+        TrainingLabel = TestLabels(i);
+        Layer1Outputs = horzcat(pt_atan(Layer1Weights*Im),1)';
+        FinalOutputs = FinalWeights*Layer1Outputs;
+    	Class = find(FinalOutputs == max(FinalOutputs))-1;% the -1 because index 1 corresponds to 
+    	% classification as a 0
+    	if(Class==TrainingLabel) %#ok<ALIGN>
+        	counter = counter+1;
+        end
+        end
+        acc=counter/TestSize
+        PartialAcc(testnum)=acc;
+    end %End test
+end %End train loop
+    
+    
 
-%Now we test the accuracy of the trained network
-counter =0;
-for i=1:TestSize
-	Im = horzcat(TestImages(:,i)',1)';
-	TrainingLabel = TestLabels(i);
-	Layer1Outputs = horzcat(pt_atan(Layer1Weights*Im),1)';
-	FinalOutputs = FinalWeights*Layer1Outputs;
-	Class = find(FinalOutputs == max(FinalOutputs))-1;% the -1 because index 1 corresponds to 
-	% classification as a 0
-	if(Class==TrainingLabel)
-		counter = counter+1;
-	end
-end
-Accuracy = counter/TestSize;
+
+
+%Final diagnostic plots
+Accuracy = counter/TestSize; %Final Accuracy
+DisplayImage(Layer1Weights(1,:)); 
+DisplayImage(Layer1Weights(2,:)); 
+DisplayImage(Layer1Weights(3,:)); 
